a12681e249179f7d1af72bad9307aa38d15eb3fc
Ian Stride <ianstride@gmail.com>
2024-07-29 17:32:45 +0100

    Standardise file paths of items extracted from emails
    Items extracted from emails will be stored at a location that adheres to the
    convention: {path_prefix}/{date}/{filename}. The path prefix is configurable at
    the level of the extractor i.e. each one can have a different prefix. The format
    of the date is '%Y%m%d. For items extracted from HTML, the filename is the
    subject of the email, slugified. For items that are attachments, the filename is
    the same as the attachment, but slugified.

--- src/mailingester/extractors.py
+++ src/mailingester/extractors.py
@@ -1,102 +1,57 @@
-import re
-from datetime import datetime
 from pathlib import Path
 
-from mailingester.models import Config, Content, Email
+from slugify import slugify
+
+from mailingester.models import Content, Email
 
 
 class Extractor:
 
-    def __init__(self, allowed: list[str]):
+    def __init__(self, allowed: list[str], path_prefix: str = ""):
         self.allowed_addresses = allowed
+        self.path_prefix = Path(path_prefix)
 
     def can_extract(self, email: Email) -> bool:
         return any(address in email.sender for address in self.allowed_addresses)
 
 
 class ZambiaExtractor(Extractor):
+    FOOTER_START = "-- <br />\nYou received this message"
 
     def extract(self, email: Email) -> list[Content]:
-        items = []
-        config = Config(
-            template="{0}/{3}s/{date}",
-            date_template="{5}/{6}/{7}",
-            date_format_in="%d/%m/%Y",
-            date_format_out="%Y_%m_%d",
-            footer_start="-- <br />\nYou received this message",
-        )
-        path = text_to_path(config, email.subject)
         html = email.html
 
         if html:
-            html.filename = "html" / path.with_suffix(".html")
+            html.filename = (
+                self.path_prefix
+                / email.date.strftime("%Y%m%d")
+                / slugify(email.subject)
+            ).with_suffix(".html")
             html.data = bytes(
                 strip_footer(
-                    config.footer_start,
+                    self.FOOTER_START,
                     convert_line_endings(str(html.data, "utf-8")),
                 ),
                 "utf-8",
             )
-            items.append(html)
 
-        for item in email.attachments:
-            item.filename = "attachments" / path / item.filename
-            items.append(item)
-
-        return items
+        return [html]
 
 
 class MalawiExtractor(Extractor):
-
-    def __init__(self, path_prefix, **kwargs):
-        super().__init__(**kwargs)
-        self.path_prefix = Path(path_prefix)
 
     def extract(self, email: Email) -> list[Content]:
         items = []
 
         for item in email.attachments:
-            path = email.date.strftime("%Y_%m_%d")
-            filename = self.translate_filename(str(item.filename))
-
-            item.filename = self.path_prefix / path / filename
+            item.filename = (
+                self.path_prefix
+                / email.date.strftime("%Y%m%d")
+                / item.filename.with_stem(slugify(item.filename.stem))
+            )
             items.append(item)
 
         return items
-
-    def translate_filename(self, filename: str):
-        translation_map = {
-            "forecast": "forecast",
-            "morning": "bulletin_morning",
-            "evening": "bulletin_evening",
-        }
-        lower = filename.lower()
-
-        for key, stem in translation_map.items():
-            if key in lower:
-                return stem + ".pdf"
-
-        raise Exception("Filename translation failed")
-
-
-def text_to_path(config: Config, text: str) -> Path:
-    tokens = [t.lower() for t in re.findall(config.regex, text) if t]
-
-    if not tokens:
-        raise Exception(
-            {
-                "msg": "No tokens produced",
-                "subject": text,
-                "regex": config.regex,
-            }
-        )
-
-    date = datetime.strptime(
-        config.date_template.format(*tokens),
-        config.date_format_in,
-    ).strftime(config.date_format_out)
-
-    return Path(config.template.format(*tokens, date=date))
 
 
 def convert_line_endings(text: str):
